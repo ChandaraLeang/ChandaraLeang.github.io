5450d5193ed9dbdf802c44526066faa5d46476b1
ChandaraLeang <leangchandara@gmail.com>
2017-03-31 23:17:13 -0500

    changing some structure such as addind boodstrap to be responsive ...

--- Js/slideshow.js
+++ Js/slideshow.js
@@ -19,6 +19,6 @@ window.onload = function() {
 		}
 		slides[slideIndex-1].style.display = "block";  
 		dots[slideIndex-1].className += " active";
-		setTimeout(showSlides, 5000);
+		setTimeout(showSlides, 2500);
 	};
 }
